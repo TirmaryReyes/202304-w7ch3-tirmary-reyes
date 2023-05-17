6f45e37fc80039a1d3535668bb91c51c9549715c
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-17 22:25:36 +0200

    Add in database the file index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const allowedOrigins = ["http://localhost:4000"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(cors(options));
 
 export default app;
