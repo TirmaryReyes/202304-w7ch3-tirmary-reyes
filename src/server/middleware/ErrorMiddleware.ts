40f200d37a80cb706c48d1c7d1aa30a9413a932e
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-17 22:22:38 +0200

    Add CustomError and middleware

---
+++ src/server/middleware/ErrorMiddleware.ts
@@ -0,0 +1,46 @@
+import createDebug from "debug";
+import { type NextFunction, type Request, type Response } from "express";
+import CustomError from "../../CustomError/CustomError";
+import { ValidationError } from "express-validation";
+import chalk from "chalk";
+
+const debug = createDebug("items-api:server:middleware:errorControllers");
+
+export const notFoundError = (
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  const error = new CustomError(404, "Endpoint not found", "Not found");
+
+  next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (error instanceof ValidationError) {
+    const validationErrors = error.details
+
+      .body!.map((joiError) => joiError.message)
+      .join(" & ");
+
+    error.publicMessage = validationErrors;
+
+    debug(chalk.blue(validationErrors));
+
+    debug(validationErrors);
+  }
+
+  debug(error.message);
+
+  const statusCode = error.statusCode || 500;
+  const messageError = error.statusCode ? error.publicMessage : "General error";
+
+  res.status(statusCode).json({ messageError });
+};
+
+export default debug;
