2dba6b4ecd895b453e0395bda738f34c2bfd6715
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-19 08:52:45 +0200

    Add index.ts middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import userRouter from "./routes/user/userRouter";
+import { generalError, notFoundError } from "./middleware/ErrorMiddleware";
 
 const allowedOrigins = process.env.ALLOWEN_ORIGIN;
 
@@ -13,6 +15,12 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/user", userRouter);
+
+app.use(notFoundError);
+
+app.use(generalError);
 
 app.use(cors(options));
 
